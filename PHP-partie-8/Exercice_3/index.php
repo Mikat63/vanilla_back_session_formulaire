5ae7dc2eb90487079567b0379c7a257e9b1ce715
mikaT <m.tores63@outlook.fr>
2025-12-08 09:06:53 +0100

    COMPLETED : PHP-partie-8 Exercice 3

--- PHP-partie-8/Exercice_3/index.php
+++ PHP-partie-8/Exercice_3/index.php
@@ -9,54 +9,60 @@
 </head>
 
 <body>
-    <div>
-        <form action="process/user.php" method="POST">
+    <header>
+        <h1>PHP partie 8 - Exercice 3</h1>
+    </header>
+    <main>
+        <div class="form_container">
+            <form action="process/user.php" method="POST">
+                <div>
+                    <label for="login">Identifiant :</label>
+                    <input type="text" name="login" id="login" placeholder="Entrez votre nom" minlength="3" maxlength="30" required>
+                </div>
+                <br>
+                <div>
+                    <label for="password">Mot de passe :</label>
+                    <input type="password" name="password" id="password" placeholder="Entrez mot de passe" minlength="3" maxlength="30" required>
+                </div>
+                <br>
+                <div>
+                    <button class="button_validate" type="submit">Valider</button>
+                </div>
+            </form>
             <div>
-                <label for="login">Identifiant :</label>
-                <input type="text" name="login" id="login" placeholder="Entrez votre nom" minlength="3" maxlength="30" required>
-            </div>
-            <br>
-            <div>
-                <label for="password">Mot de passe :</label>
-                <input type="password" name="password" id="password" placeholder="Entrez mot de passe" minlength="3" maxlength="30" required>
-            </div>
-            <br>
-            <div>
-                <button type="submit">Valider</button>
-            </div>
-        </form>
-        <div>
-            <?php
-            if (isset($_GET['success'])) {
-                echo "<p class='green'>Connexion réussie</p>";
-            } else if (isset($_GET['error'])) {
-                switch (($_GET['error'])) {
-                    case 'bad_method':
-                        echo "<p class='red'>Méthode incorrecte</p>";
-                        break;
-                    case 'missing':
-                        echo "<p class='red'>Tous les champs sont requis</p>";
-                        break;
-                    case 'empty':
-                        echo "<p class='red'>Les champs ne peuvent être vides</p>";
-                        break;
-                    case 'min':
-                        echo "<p class='red'>Les champs doivent avoir minimum 3 caractères</p>";
-                        break;
-                    case 'max':
-                        echo "<p class='red'>Les champs doivent avoir maximum 30 caractères</p>";
-                        break;
-                    case 'password':
-                        echo "<p class='red'>Le mot de passe doit contenir 8 caractères, 1 majuscule, un chiffre et 1 caractère spécial</p>";
-                        break;
-                    default:
-                        echo "Erreur inconnue";
-                        break;
+                <?php
+                if (isset($_GET['success'])) {
+                    echo "<p class='green'>Connexion réussie</p>";
+                } else if (isset($_GET['error'])) {
+                    switch (($_GET['error'])) {
+                        case 'bad_method':
+                            echo "<p class='red'>Méthode incorrecte</p>";
+                            break;
+                        case 'missing':
+                            echo "<p class='red'>Tous les champs sont requis</p>";
+                            break;
+                        case 'empty':
+                            echo "<p class='red'>Les champs ne peuvent être vides</p>";
+                            break;
+                        case 'min':
+                            echo "<p class='red'>Les champs doivent avoir minimum 3 caractères</p>";
+                            break;
+                        case 'max':
+                            echo "<p class='red'>Les champs doivent avoir maximum 30 caractères</p>";
+                            break;
+                        case 'password':
+                            echo "<p class='red'>Le mot de passe doit contenir 8 caractères, 1 majuscule, un chiffre et 1 caractère spécial</p>";
+                            break;
+                        default:
+                            echo "Erreur inconnue";
+                            break;
+                    }
                 }
-            }
-            ?>
+                ?>
+            </div>
         </div>
-    </div>
+    </main>
+
 </body>
 
 </html>
